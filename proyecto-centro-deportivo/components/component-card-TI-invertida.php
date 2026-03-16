40dd6ed4b2d067424d5d599f8f0f08c1e053a5c4
javichut00 <jcuadrosgarcia@educa.madrid.org>
2026-03-16 14:36:56 +0100

    Terminada la pagina de montaña, aunque está sujeta a posibles cambios

--- proyecto-centro-deportivo/components/component-card-TI-invertida.php
+++ proyecto-centro-deportivo/components/component-card-TI-invertida.php
@@ -1,4 +1,4 @@
-<section class="hero-section py-5">
+<<section class="hero-section py-5">
     <div class="container seccion-montaña">
         <div class="row align-items-center g-4">
 
@@ -9,12 +9,12 @@
             <!-- Imagen -->
             <div class="col-12 col-md-6">
                 <div class="hero-img-wrapper">
-                    <img src="<?= $imagen ?>" alt="<?= $titulo ?>" class="img-fluid rounded">
+                    <img src="<?= $imagen ?>" alt="<?= $titulo ?>" class="img-fluid rounded <?= $claseExtra ?>">
                 </div>
             </div>
 
             <!-- Texto -->
-            <div class="col-12 col-md-6 text-md-end ms-md-auto">
+            <div class="col-12 col-md-6 text-md-start ms-md-auto">
                 <h1 class="hero-title"><?= $titulo ?></h1>
 
                 <p class="hero-text mt-3">
